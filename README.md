ad245904b45b5e4103b66398e74a54a6ce962806
Antonio <mr.antonsilin@gmail.com>
2023-01-30 18:00:09 +0300

    gitignore

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # py-fancy-tables
 Fancy tables for python console
+
+https://en.wikipedia.org/wiki/Box-drawing_character#Box_Drawing
--- fancy_table/fancy_table.py
+++ fancy_table/fancy_table.py
@@ -8,7 +8,12 @@ def colorize(text, color, colors_enabled):
         return text
 
 
-class Section:
+class TableRowBase:
+    def to_string(self, prev, next, colors_enabled):
+        raise NotImplementedError()
+
+
+class Section(TableRowBase):
     name = None
     _table: 'FancyTable' = None
 
@@ -16,7 +21,7 @@ class Section:
         self.name = name
         self._table = table
 
-    def to_string(self, colors_enabled):
+    def to_string(self, prev, next, colors_enabled):
         def border(t):
             return colorize(t, Fore.LIGHTBLACK_EX, colors_enabled)
 
@@ -28,31 +33,26 @@ class Section:
         return result
 
 
-class Row:
-    row = None
+class Row(TableRowBase):
+    row: list = None
     _table: 'FancyTable' = None
 
     def __init__(self, row, table):
         self.row = row
         self._table = table
 
-    def to_string(self, colors_enabled):
+    def to_string(self, prev=None, next=None, colors_enabled):
         def border(t):
             return colorize(t, Fore.LIGHTBLACK_EX, colors_enabled)
 
         widths = self._table.widths
         result = list()
-        result.append(border('┠─' + '─┴─'.join(['─'*w for w in widths]) + '─┨'))
-        result.append(border('┃ ') + self.name.center(sum(widths) + 3 * (len(widths) - 1)) + border(' ┃'))
-        result.append(border('┠─' + '─┬─'.join(['─' * w for w in widths]) + '─┨'))
+        result.append(
+            border('┃ ') +
+            border(' │ ').join([td.center(widths[idx]) for idx, td in enumerate(self.row)]) +
+            border(' ┃')
+        )
         return result
-
-
-class Row:
-    fields = None
-
-    def __init__(self, fields):
-        self.fields = fields
 
 
 class FancyTable:
@@ -73,7 +73,7 @@ class FancyTable:
         for idx, field in enumerate(row):
             if self.widths[idx] < len(field):
                 self.widths[idx] = len(field)
-        self.rows.append(row)
+        self.rows.append(Row(row, self))
 
     def add_section(self, section_name):
         self.rows.append(Section(section_name, self))
@@ -92,13 +92,17 @@ class FancyTable:
         header = border('┃ ') + border(' │ ').join(header) + border(' ┃')
         result.append(header)
 
-        result.append(border('┣━' + '━┿━'.join(['━' * w for w in self.widths]) + '━┫'))
+        if type(self.rows[0]) == Row:
+            result.append(border('┣━' + '━┿━'.join(['━' * w for w in self.widths]) + '━┫'))
+        else:
+            result.append(border('┣━' + '━┷━'.join(['━' * w for w in self.widths]) + '━┫'))
 
-        for row in self.rows:
-            if type(row) == Section:
-                result += row.to_string(colors_enabled)
-            else:
-                result.append(border('┃ ') + border(' │ ').join([td.center(self.widths[idx]) for idx, td in enumerate(row)]) + border(' ┃'))
+        for idx, row in enumerate(self.rows):
+            result += row.to_string(
+                row[idx-1] if idx > 0 else None,
+                row[idx+1] if idx < len(self.rows)-1 else None,
+                colors_enabled
+            )
 
         result.append(border('┗━' + '━┷━'.join(['━' * w for w in self.widths]) + '━┛'))
         return '\n'.join(result)
